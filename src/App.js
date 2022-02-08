427abb4c693c6ba0ec1d7f9ae37ac4db7a8a1422
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-02-08 16:55:11 +0100

    Add code for exercise solutions

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import styled from "styled-components/macro";
 function App() {
   return (
     <AppContainer>
-      <h1>ClientBoard</h1>
-      <CardGrid>
+      <Title>ClientBoard</Title>
+      <CardGrid columns={3}>
         {USERS.map((user) => (
-          <Card user={user} />
+          <Card key={user.id} userData={user} />
         ))}
       </CardGrid>
     </AppContainer>
@@ -18,8 +18,6 @@ function App() {
 export default App;
 
 const AppContainer = styled.main`
-  background-color: #12121a;
-  color: #ccd;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -27,7 +25,13 @@ const AppContainer = styled.main`
 
 const CardGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr); // 1fr 1fr 1fr 1fr
+  grid-template-columns: ${(props) => "repeat(" + props.columns + ",1fr)"};
   gap: 30px;
   padding: 20px;
+`;
+
+const Title = styled.h1`
+  border-top: 5px solid #ccd;
+  border-bottom: 5px solid #ccd;
+  border-radius: 5px;
 `;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
-function Card({ user }) {
+const buttonStyles = {
+  regular: "#141417",
+  danger: "red",
+  success: "green",
+};
+
+function Card({ userData }) {
   return (
-    <CardContainer>
-      <h2>{user.profile.name}</h2>
-      <p>{user.profile.about}</p>
-      <button>contact</button>
-      <button>delete</button>
+    <CardContainer roles={userData.roles}>
+      <Company>{userData.profile.company}</Company>
+      <h2>{userData.profile.name}</h2>
+      <p>{userData.profile.about}</p>
+      <Button styling="regular">contact</Button>
+      <Button styling="danger">delete</Button>
     </CardContainer>
   );
 }
@@ -17,9 +24,18 @@ const CardContainer = styled.article`
   padding: 20px;
   background-color: #335;
   border-radius: 14px;
-  border-left: 14px solid #ccd;
+  border-left: ${(props) => {
+    return props.roles.includes("admin")
+      ? "14px solid hotpink"
+      : "14px solid #ccd";
+  }};
   opacity: 0.8;
   transition: 0.2s;
+
+  h2 {
+    font-size: 1.6rem;
+    color: ${(props) => (props.roles.includes("admin") ? "hotpink" : "#ccd")};
+  }
 
   p {
     color: white;
@@ -27,5 +43,27 @@ const CardContainer = styled.article`
 
   &:hover {
     opacity: 1;
+  }
+`;
+
+const Company = styled.span`
+  border: 2px solid #ccd;
+  padding: 5px 14px;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => buttonStyles[props.styling]};
+  color: white;
+  padding: 20px 30px;
+  border: none;
+  border-radius: 14px;
+  margin: 4px;
+
+  &:hover {
+    background-color: #444;
+  }
+
+  &:active {
+    transform: scale(0.95);
   }
 `;
